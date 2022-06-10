19f4b4d7a65873cfab86199db359eec7ad39698e
Danielle Lyle <karendlyle@gmail.com>
2022-06-10 13:10:11 +0200

    source for keys and sound src

---
+++ src/components/ButtonList.js
@@ -0,0 +1,38 @@
+export const ButtonList = [
+	{
+		key: 'Q',
+		sound: './sfx/hh-open.mp3'
+	},
+	{
+		key: 'W',
+		sound: './sfx/hh-closed.mp3'
+	},
+	{
+		key: 'E',
+		sound: './sfx/hh-pedal.mp3'
+	},
+	{
+		key: 'A',
+		sound: './sfx/kick.mp3'
+	},
+	{
+		key: 'S',
+		sound: './sfx/snare.mp3'
+	},
+	{
+		key: 'D',
+		sound: './sfx/tom.mp3'
+	},
+	{
+		key: 'Z',
+		sound: './sfx/ride.mp3'
+	},
+	{
+		key: 'X',
+		sound: './sfx/crash.mp3'
+	},
+	{
+		key: 'C',
+		sound: './sfx/rim.mp3'
+	}
+];
