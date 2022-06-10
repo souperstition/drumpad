fcd833cb44d0e6cd5a2a5412a43c9295e0878b39
Danielle Lyle <karendlyle@gmail.com>
2022-06-10 13:09:40 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Center } from '@chakra-ui/react';
+import DrumPad from './components/DrumPad';
+import Button from './components/Button';
+
 function App() {
-	return <div className="App" />;
+	return (
+		<Center h="100vh" id="drum-machine">
+			<DrumPad />
+		</Center>
+	);
 }
 
 export default App;
