8c4946d42a311c581457b3f7a8adec0f8fdfb4f7
Danielle Lyle <karendlyle@gmail.com>
2022-06-13 09:12:16 +0200

    change pad layout

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,47 +1,47 @@
 export const ButtonList = [
 	{
 		key: 'Q',
-		sound: `${process.env.PUBLIC_URL}/sfx/kick.mp3`,
-		desc: 'Kick'
-	},
-	{
-		key: 'W',
-		sound: `${process.env.PUBLIC_URL}/sfx/snare.mp3`,
-		desc: 'Snare'
-	},
-	{
-		key: 'E',
-		sound: `${process.env.PUBLIC_URL}/sfx/rim.mp3`,
-		desc: 'Rimshot'
-	},
-	{
-		key: 'A',
-		sound: `${process.env.PUBLIC_URL}/sfx/ride.mp3`,
-		desc: 'Ride'
-	},
-	{
-		key: 'S',
-		sound: `${process.env.PUBLIC_URL}/sfx/crash.mp3`,
-		desc: 'Crash'
-	},
-	{
-		key: 'D',
-		sound: `${process.env.PUBLIC_URL}/sfx/hh-pedal.mp3`,
-		desc: 'HH Pedal'
-	},
-	{
-		key: 'Z',
 		sound: `${process.env.PUBLIC_URL}/sfx/tom.mp3`,
 		desc: 'Tom'
 	},
 	{
-		key: 'X',
+		key: 'W',
 		sound: `${process.env.PUBLIC_URL}/sfx/hh-closed.mp3`,
 		desc: 'Closed HH'
 	},
 	{
-		key: 'C',
+		key: 'E',
 		sound: `${process.env.PUBLIC_URL}/sfx/hh-open.mp3`,
 		desc: 'Open HH'
+	},
+	{
+		key: 'A',
+		sound: `${process.env.PUBLIC_URL}/sfx/snare.mp3`,
+		desc: 'Snare'
+	},
+	{
+		key: 'S',
+		sound: `${process.env.PUBLIC_URL}/sfx/rim.mp3`,
+		desc: 'Rimshot'
+	},
+	{
+		key: 'D',
+		sound: `${process.env.PUBLIC_URL}/sfx/ride.mp3`,
+		desc: 'Ride'
+	},
+	{
+		key: 'Z',
+		sound: `${process.env.PUBLIC_URL}/sfx/kick.mp3`,
+		desc: 'Kick'
+	},
+	{
+		key: 'X',
+		sound: `${process.env.PUBLIC_URL}/sfx/hh-pedal.mp3`,
+		desc: 'HH Pedal'
+	},
+	{
+		key: 'C',
+		sound: `${process.env.PUBLIC_URL}/sfx/crash.mp3`,
+		desc: 'Crash'
 	}
 ];
